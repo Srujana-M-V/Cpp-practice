99d25a5fd1716c37834383131b6664c7b10d58bb
sgr_srujana <srujanamv123@gmail.com>
2026-03-30 04:53:21 +0530

    added dynamic reverse alphabet triangle pattern using character arithmetic in C++

--- Patterns/pattern18.cpp
+++ Patterns/pattern18.cpp
@@ -1,31 +1,16 @@
 #include <iostream>
 using namespace std;
-
-void pattern17(int n){
+void pattern18(int n){
     for(int i=0; i<n; i++){
-
-        // spaces
-        for(int j=0; j<n-i-1; j++){
-            cout << " ";
+         for(char ch = ('A' + n - 1) - i; ch <= ('A' + n - 1); ch++){
+            cout << ch << " ";
         }
-
-        // increasing characters
-        for(int j=0; j<=i; j++){
-            cout << char('A' + j);
-        }
-
-        // decreasing characters
-        for(int j=i-1; j>=0; j--){
-            cout << char('A' + j);
-        }
-
-        cout << endl;
+        cout<<endl;
     }
 }
-
 int main(){
     int n;
-    cin >> n;
-    pattern17(n);
+    cin>>n;
+    pattern18(n);
     return 0;
 }
