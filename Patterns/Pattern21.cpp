63c664ef46752d7d306108f2181518659f93f877
sgr_srujana <srujanamv123@gmail.com>
2026-03-31 17:09:55 +0530

    added hollow butterfly star pattern using nested loops in C++

---
+++ Patterns/Pattern21.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void pattern20(int n){
+
+    // upper
+    for(int i=0; i<n; i++){
+        for(int j=0; j<2*n; j++){
+            if(j < n-i || j >= n+i){
+                if(j==0 || j==2*n-1 || j==n-i-1 || j==n+i){
+                    cout<<"*";
+                } else cout<<" ";
+            } else cout<<" ";
+        }
+        cout<<endl;
+    }
+
+    // lower
+    for(int i=n-2; i>=0; i--){
+        for(int j=0; j<2*n; j++){
+            if(j < n-i || j >= n+i){
+                if(j==0 || j==2*n-1 || j==n-i-1 || j==n+i){
+                    cout<<"*";
+                } else cout<<" ";
+            } else cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    pattern20(n);
+}
